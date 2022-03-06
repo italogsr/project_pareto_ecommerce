fc210f8946a323e8d969782f2607c9a5fc591e72
Italo Ribeiro <italogsr@gmail.com>
2022-03-06 20:15:33 -0300

    add Itemload

--- gdg_scrapy/gdg_scrapy/items.py
+++ gdg_scrapy/gdg_scrapy/items.py
@@ -6,23 +6,62 @@
 import scrapy
 from scrapy.loader import ItemLoader
 from itemloaders.processors import TakeFirst, MapCompose
-from w3lib.html import remove_tags
 
 def remove_moeda(valor):
     return valor.replace('R$', '').strip()
 
 
+def remove_valor_riscado(valor_riscado):
+    return valor_riscado.replace('de R$', '').strip()
+
+
+def remove_ref(valor_raw):
+    return valor_raw.replace('Ref:','').strip()
+
+
+def limpa_desconto(valor):
+    return valor.replace('%', '').strip()
+
+
+def limpa_parcelas(condicao):
+    return condicao.strip()
+
+
 class GdgScrapyItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field()
-    url = scrapy.Field()
-    ref = scrapy.Field()
-    nome = scrapy.Field()
-    imagem = scrapy.Field()
-    valor_riscado = scrapy.Field()
-    valor_boleto = scrapy.Field()
-    parcelas =  scrapy.Field()
-    desconto = scrapy.Field()
-    categoria = scrapy.Field()
+    name = scrapy.Field(output_processor=TakeFirst())
+    
+    url = scrapy.Field(output_processor=TakeFirst())
+    
+    ref = scrapy.Field(
+        input_processor=MapCompose(remove_ref),
+        output_processor=TakeFirst()
+    )
+    
+    nome = scrapy.Field(output_processor=TakeFirst())
+    
+    imagem = scrapy.Field(output_processor=TakeFirst())
+    
+    valor_riscado = scrapy.Field(
+        input_processor=MapCompose(remove_valor_riscado),
+        output_processor=TakeFirst()
+    )
+    
+    valor_boleto = scrapy.Field(
+        input_processor=MapCompose(remove_moeda),
+        output_processor=TakeFirst()
+    )
+    
+    parcelas =  scrapy.Field(
+        input_processor=MapCompose(limpa_parcelas),
+        output_processor=TakeFirst()
+    )
+    
+    desconto = scrapy.Field(
+        input_processor=MapCompose(limpa_desconto),
+        output_processor=TakeFirst()
+    )
+    
+    categoria = scrapy.Field(output_processor=TakeFirst())
     
 
--- gdg_scrapy/gdg_scrapy/spiders/gdg_home.py
+++ gdg_scrapy/gdg_scrapy/spiders/gdg_home.py
@@ -1,6 +1,7 @@
 from email.policy import default
 import scrapy
 from gdg_scrapy.items import GdgScrapyItem
+from itemloaders import ItemLoader
 
 class GdgHomeSpider(scrapy.Spider):
     name = 'gdg_home'
@@ -24,25 +25,43 @@ class GdgHomeSpider(scrapy.Spider):
             )
 
     def parse_category(self, response):
+        
         item = GdgScrapyItem()
         
+        
         #Nome das Categorias             
-        item['categoria'] = response.xpath(            
+        categoria = response.xpath(            
             '//ol[@class="container-flex"]/li[last()]/span/strong/text()'
         ).extract_first()        
                       
         #Coletar Produtos
         
         for produto in response.xpath('//ul[@class="grid-list"]/li'):
+            loader = ItemLoader(item=GdgScrapyItem(),
+                                selector = produto)
+
+
+            loader.add_xpath('url', './/meta[@itemprop="url"]/@content')
+            loader.add_xpath('desconto', './/p[@class ="content__discount"]/text()')
+            loader.add_xpath('valor_boleto', './/p[@class="content__price"]/span[3]/text()')        
+            loader.add_xpath('nome', './/h2/text()')
+            loader.add_xpath('imagem', './/div[@class="product-tile--container__figure"]//img/@src')
+            loader.add_css('valor_riscado', '.mini-letter span ::text')            
+            loader.add_css('parcelas', '.content__installments ::text')
+            loader.add_css('ref', '.content__ref ::text')
+            loader.add_value('categoria', categoria)
             
-            item['url'] = produto.xpath('.//meta[@itemprop="url"]/@content').extract_first()
-            item['ref'] = produto.css('.content__ref ::text').get()
-            item['nome']= produto.xpath('.//h2/text()').extract_first()
-            item['imagem'] = produto.xpath('.//div[@class="product-tile--container__figure"]//img/@src').extract_first()
-            item['valor_riscado'] = response.css(".mini-letter span ::text").get()
-            item['valor_boleto'] = produto.xpath('.//p[@class="content__price"]/span[3]/text()').extract_first()
-            item['parcelas'] =  produto.css(".content__installments ::text").get()
-            item['desconto'] = produto.xpath('.//p[@class ="content__discount"]/text()').extract_first()
+            item = loader.load_item()
+            
+            
+            #item['url'] = produto.xpath('.//meta[@itemprop="url"]/@content').extract_first()
+            #item['ref'] = produto.css('.content__ref ::text').get()
+            #item['nome']= produto.xpath('.//h2/text()').extract_first()
+            #item['imagem'] = produto.xpath('.//div[@class="product-tile--container__figure"]//img/@src').extract_first()
+            #item['valor_riscado'] = response.css(".mini-letter span ::text").get()
+            #item['valor_boleto'] = produto.xpath('.//p[@class="content__price"]/span[3]/text()').extract_first()
+            #item['parcelas'] =  produto.css(".content__installments ::text").get()
+            #item['desconto'] = produto.xpath('.//p[@class ="content__discount"]/text()').extract_first()
             
             yield item   
         
